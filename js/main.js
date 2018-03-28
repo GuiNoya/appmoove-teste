b5a47ad0b01969ff64c59ba6c9b0cabf2314e15a
Guilherme Noya <gui.noya@gmail.com>
2018-03-28 20:21:31 -0300

    Add details to modal window

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 "use strict";
 
 var apiKey = "9da26ae1350cf4c9c3501f0257e754dd";
-var moviesToShow = 5; // Up to 20 (1 page at TMDb API)
+var moviesToShowInTheaters = 3; // Up to 20 (1 page at TMDb API)
+var moviesToShowPopular = 5;
+var moviesToShowSearch = 9;
 
 var tmdbEndpoint = "https://api.themoviedb.org/3";
-
 
 var inTheatersButton = document.querySelector("#in-theaters");
 var popularsButton = document.querySelector("#populars");
 var searchbox = document.querySelector("#searchbox");
 var modal = document.querySelector("#modal-container");
+var modalContent = document.querySelector(".modal-content");
 var modalCloseButton = document.querySelector(".close");
+var modalPoster = document.querySelector("#modal-poster");
+var modalTitle = document.querySelector("#modal-title");
+var modalDate = document.querySelector("#modal-date");
+var modalGenres = document.querySelector("#modal-genres");
 
 modalCloseButton.onclick = function () {
     modal.style.opacity = 0;
@@ -29,16 +35,16 @@ function openPopular() {
     callTmdbEndpoint(generateRequestURI("popular"), function (response) {
         inTheatersButton.className = "";
         popularsButton.classList = "active";
-        refreshMoviesList(JSON.parse(response).results, moviesToShow);
+        refreshMoviesList(JSON.parse(response).results, moviesToShowInTheaters);
     });
 }
 
 function openInTheaters() {
-    if (document.querySelector("a.active") === inTheatersButton) return;    
+    if (document.querySelector("a.active") === inTheatersButton) return;
     callTmdbEndpoint(generateRequestURI("now_playing"), function (response) {
         inTheatersButton.className = "active";
         popularsButton.className = "";
-        refreshMoviesList(JSON.parse(response).results, moviesToShow);
+        refreshMoviesList(JSON.parse(response).results, moviesToShowPopular);
     });
 }
 
@@ -53,18 +59,21 @@ function searchMovie() {
         }
         inTheatersButton.className = "";
         popularsButton.className = "";
-        refreshMoviesList(tmdbResults, moviesToShow);
+        refreshMoviesList(tmdbResults, moviesToShowSearch);
     });
 }
 
 function generateRequestURI(request, query) {
     var uri = tmdbEndpoint;
     if (request === "search") {
-        uri += "/search/movie?include_adult=false&query=" + encodeURIComponent(query) + "&";
+        uri += "/search/movie?include_adult=false&query=" +
+            encodeURIComponent(query) + "&page=1&region=BR&";
+    } else if (request === "movie") {
+        uri += "/movie/" + query + "?";
     } else {
-        uri += "/movie/" + request + "?";
+        uri += "/movie/" + request + "?page=1&region=BR&";
     }
-    return uri + "page=1&region=BR&language=pt-BR&api_key=" + apiKey;
+    return uri + "language=pt-BR&api_key=" + apiKey;
 }
 
 function callTmdbEndpoint(requestURI, callback) {
@@ -88,11 +97,11 @@ function callTmdbEndpoint(requestURI, callback) {
 
 function refreshMoviesList(tmdbResults, qty) {
     var newItems = "";
-    for (var i = 0; i < qty; i++) {
+    for (var i = 0; i < qty && i < tmdbResults.length; i++) {
         newItems += '<div class="movie-general" id="movie' + i + '" onclick=showMovieDetails(' + tmdbResults[i].id + ')>' +
             '<img class="movie-img" src="http://image.tmdb.org/t/p/w185' + tmdbResults[i].poster_path + '" alt="Poster ' + tmdbResults[i].title + '">' +
             '<p class="movie-name">' + tmdbResults[i].title + '</p>' +
-            '<p>Estréia: ' + invertDateString(tmdbResults[i].release_date) + '</p>' +
+            '<p>Estreia: ' + invertDateString(tmdbResults[i].release_date) + '</p>' +
             '</div>\n';
     }
     document.querySelector("main").innerHTML = newItems;
@@ -106,4 +115,25 @@ function invertDateString(dateString) {
 function showMovieDetails(id) {
     modal.style.opacity = 1;
     modal.style.visibility = "visible";
+    callTmdbEndpoint(generateRequestURI("movie", id), function (response) {
+        var movie = JSON.parse(response);
+        console.log(movie);
+        modalTitle.innerHTML = movie.title;
+        modalDate.innerHTML = "Data de estreia: " + invertDateString(movie.release_date);
+        var genresStr = "Gêneros: ";
+        var i = 0;
+        for (; i < movie.genres.length; i++) {
+            genresStr += movie.genres[i].name + ", ";
+        }
+        modalGenres.innerHTML = i === 0 ? "nenhum" : genresStr.substr(0, genresStr.length - 2);
+        modalPoster.src = "http://image.tmdb.org/t/p/w342" + movie.poster_path;
+        if (movie.backdrop_path !== null) {
+            modalContent.style.background = "linear-gradient(rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.75)), url(http://image.tmdb.org/t/p/w780/" + movie.backdrop_path + ")";
+            modalContent.style.backgroundRepeat = "no-repeat";
+            modalContent.style.backgroundSize = "cover";
+        } else {
+            modalContent.style.backgroundImage = "";
+            modalContent.style.backgroundColor = "rgb(48, 48, 48)";
+        }
+    });
 }
